8d3e4a0b8b20aba2d285784f2121235a6d6cff4d
quartern <quartern@users.noreply.github.com>
2022-11-27 21:50:51 -0500

    Add Other Languages cat to V2

--- v2fmt.sh
+++ v2fmt.sh
@@ -5,7 +5,7 @@
 # to just the main categories
 jq -r '
 
-.Categories = [ .Categories[3,2,1] ] |
+.Categories = [ .Categories[3,2,1,6] ] |
 del(.Categories[].Items[].StreamUrls[] | select(contains("dailymotion") or contains("MAKO")))
 
 ' data/data.json > data/v2-data.json
